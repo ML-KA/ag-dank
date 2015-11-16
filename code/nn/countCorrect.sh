8a3476283900031ad8e35040f9bd22a454a2c520
phiresky <phireskyde+git@gmail.com>
2015-11-16 02:17:07 +0100

    add nn graph

--- code/nn/countCorrect.sh
+++ code/nn/countCorrect.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 expected=../../data/bits/output
+
 echo 'out of 10k:'
 echo '- correct when assuming 1:'
 cat "$expected"|tail -n 10000|grep -P '^1$'|wc -l
 echo '- correct when assuming 0:'
 cat "$expected"|tail -n 10000|grep -P '^0$'|wc -l
-
 echo '- correct with NN:'
+th src/nnetwork/agDank/4_evaluate.lua
 paste -d= <(cat "$expected"| tail -n 10000) src/nnetwork/agDank/Data/Prod/output2.txt | grep -P '0=0|1=1' | wc -l
---
+++ code/nn/loss_plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import subprocess
+
+
+def run(x):
+    return subprocess.check_output(["bash", "-c", x])
+
+optimizer=run("cat nn.log|grep -Po '(?<=optimizer:loss avgError ).*'").decode('utf-8').strip()
+tester=run("cat nn.log|grep -Po '(?<=tester:loss avgError ).*'").decode('utf-8').strip()
+
+optimizer = list(map(float, map(str.strip, optimizer.split("\n"))))
+tester = list(map(float, map(str.strip, tester.split("\n"))))
+
+
+optimizer = np.array(optimizer)
+optimizer*=10
+tester = np.array(tester)
+
+t = np.arange(0.0, 2.0, 0.01)
+s = np.sin(2*np.pi*t)
+plt.plot(optimizer, label='optimizer')
+plt.plot(tester, label='tester')
+
+plt.xlabel('epoch')
+plt.ylabel('cross-entropy')
+#plt.yscale("log")
+plt.title('Loss of Net')
+plt.grid(True)
+plt.savefig("loss_plot.svg")
+plt.legend()
+plt.show()
